8ffa30a2f8e266d2dc13c11ccfde82448ac97018
himeshrma <sharmahimesh2003@gmail.com>
2025-12-29 02:16:56 +0530

    Implamented a Higher Order Component for managing states and add the first window -terminal

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "#components/Navbar";
 import { Welcome } from "#components/Welcome";
 import Dock from "#components/Dock";
+import WindowControl from "#components/WindowControl";
 
-export { Dock, Navbar, Welcome };
+export { Dock, Navbar, Welcome, WindowControl };
